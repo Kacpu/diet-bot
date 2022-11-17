0e6804d2a300cfb5282104a6d4695f421f8da55d
kacper <kacper.tarlowski20@gmail.com>
2022-11-17 19:37:21 +0100

    add dialog

---
+++ DietBot/Dialogs/DietDialog.cs
@@ -0,0 +1,26 @@
+﻿using DietBot.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace DietBot.Dialogs;
+
+public class DietDialog : ComponentDialog
+{
+    private readonly IStatePropertyAccessor<Diet> _dietAccessor;
+
+    public DietDialog(UserState userState) : base(nameof(DietDialog))
+    {
+        _dietAccessor = userState.CreateProperty<Diet>("Diet");
+
+        var waterfallSteps = new WaterfallStep[]
+        {
+
+        };
+
+        AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+        //AddDialog(new AttachmentPrompt(nameof(AttachmentPrompt), PicturePromptValidatorAsync));
+        AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+
+        InitialDialogId = nameof(WaterfallDialog);
+    }
+}
---
+++ DietBot/Models/Diet.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Bot.Schema;
+
+namespace DietBot.Models;
+
+public class Diet
+{
+    public string Kind { get; set; }
+
+    public Attachment Label { get; set; }
+}
