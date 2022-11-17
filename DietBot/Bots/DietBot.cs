afd3d72d323bddbeafbb20534ce7852d63824b21
kacper <kacper.tarlowski20@gmail.com>
2022-11-17 19:00:54 +0100

    Add project files.

---
+++ DietBot/Bots/DietBot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DietBot.Bots;
+
+public class DietBot : ActivityHandler
+{
+    protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+    {
+        var welcomeText = "Hello and welcome!";
+        foreach (var member in membersAdded)
+        {
+            if (member.Id != turnContext.Activity.Recipient.Id)
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+            }
+        }
+    }
+}
---
+++ DietBot/Models/Conversation.cs
@@ -0,0 +1,6 @@
+﻿namespace DietBot.States;
+
+public class Conversation
+{
+    public bool PromptedUserForName { get; set; } = false;
+}
